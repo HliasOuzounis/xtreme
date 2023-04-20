a67d75f6056965176923f03400c655749da717cc
HliasOuzounis <iliouzoun@outlook.com>
2023-04-20 18:40:39 +0300

    uni_version complete

--- Coupon Codes/uni_version.py
+++ Coupon Codes/uni_version.py
@@ -1,30 +1,6 @@
 import random
 
 
-def parser():
-    while 1:
-        data = list(input().split(" "))
-        for number in data:
-            if len(number) > 0:
-                yield (number)
-
-
-input_parser = parser()
-
-
-def get_word():
-    global input_parser
-    return next(input_parser)
-
-
-def get_number():
-    data = get_word()
-    try:
-        return int(data)
-    except ValueError:
-        return float(data)
-
-
 def generate_codes(num):
     letters = "ABCDEFGHIJK"
     digits = "012345"
@@ -77,18 +53,14 @@ class Hashtable:
         self.dulicates_found = [[] for _ in range(self.size)]
 
     def hash_function(self, value):
-        hash_value = 17
-    
+        hash_value = 0
+
         for c in value:
-            # Multiply the current hash value by a prime number
-            hash_value = hash_value * 31
-            
-            # Add the ASCII code of the current character to the hash value
             hash_value += ord(c)
-        
+            hash_value = hash_value * 17
+
         # Return the hash value
         return hash_value % self.size
-        
 
     def insert(self, value, code):
         key = self.hash_function(value)
@@ -118,39 +90,43 @@ class Hashtable:
 
     def find_duplicates_pairs(self):
         s = 0
+        print("Duplicates found: ")
         for duplicates in self.dulicates_found:
             for i, code1 in enumerate(duplicates):
                 for code2 in duplicates[i + 1 :]:
-                    print(code1, code2)
+                    for index, (char1, char2) in enumerate(zip(code1, code2)):
+                        if char1 != char2:
+                            break
+                    print(
+                        code1[:index]
+                        + "\033[44;33m" + code1[index] + "\033[0m"
+                        + code1[index + 1:],
+                        code2[:index]
+                        + "\033[44;33m" + code2[index] + "\033[0m"
+                        + code2[index + 1:],
+                    )
                     s += 1
         return s
 
 
 def main():
-    nof_codes = get_number()
+    nof_codes = 100_000
 
     codes = generate_codes(nof_codes)
-    print("Starting Calculating")
-
-    import time
-
-    t = time.time()
+    print(f"{nof_codes} codes generated\nStarting Calculating")
 
     hashtable = Hashtable(nof_codes * 12 * 2)
+    
     for i, code in enumerate(codes):
         # code = get_word()
         for index, char in enumerate(code):
             if char == "-":
                 continue
             hashtable.insert(code[:index] + "?" + code[index + 1 :], code)
-
-        # if _code > nof_codes//4: # 3.4 sec for the large examples => 14 sec total
-        #     raise EOFError()
         if i % 1000 == 0:
             print(i)
 
-    print(hashtable.find_duplicates_pairs())
-    print(time.time() - t)
+    print(f"total duplicate pairs: {hashtable.find_duplicates_pairs()}")
 
 
 if __name__ == "__main__":

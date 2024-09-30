39d158c7fc8e9f9615fe25e7a7c62305894bd2ec
HliasOuzounis <iliouzoun@outlook.com>
2024-09-30 13:55:36 +0300

    updated cheatsheet

--- cheatsheet.py
+++ cheatsheet.py
@@ -94,6 +94,68 @@ def bfs_shortest_path(graph, start_node, target_node):
     return (-1, {})
 
 
+def get_depth_and_parents(graph, root):
+    """
+    Find the depth and parents of each node in a tree
+    """
+    depths = [0] * len(graph)
+    parents = [0] * len(graph)
+    
+    visited = {root}
+    queue = [(root, 0)]
+    
+    while queue:
+        x, depth = queue.pop(0)
+        depths[x] = depth
+        
+        for child in graph[x]:
+            if child not in visited:
+                parents[child] = x
+                visited.add(child)
+                queue.append((child, depth + 1))
+                
+    return depths, parents
+
+def get_parents(graph, root, depths, first_parents):
+    """
+    Find the ancestors of each node in a tree to use in binary lifting
+    """
+    LOG = int(len(graph)**0.5 + 1)
+    parents = [[root] * LOG for node in graph]
+
+    for node in graph:
+        parents[node][0] = first_parents[node]
+    for i in range(1, LOG):
+        for node in graph:
+            parents[node][i] = parents[parents[node][i - 1]][i - 1]
+    return parents
+
+def get_lca(depth, parents, a, b):
+    """
+    Lowest Common Ancestor (LCA) using binary lifting
+    Needs a tree with parents and depths
+    LOG = ceil(log2(n)) where n is the number of nodes in the tree
+    """
+    LOG = int(len(depth)**0.5 + 1)
+    
+    if depth[a] < depth[b]:
+        a, b = b, a
+    k = depth[a] - depth[b] 
+
+    for j in range(LOG - 1, -1, -1):
+        if (k & (1<<j)):
+            a = parents[a][j]
+    
+    if a == b:
+        return a
+    
+    for j in range(LOG - 1, -1, -1):
+        if parents[a][j] != parents[b][j]:
+            a = parents[a][j]
+            b = parents[b][j]
+    
+    return parents[a][0]
+
 # ---------------- Integers ---------------- #
 
 
@@ -117,9 +179,28 @@ def gcd_Euclid(a, b):  # sourcery skip: assign-if-exp, reintroduce-else
         return a
     return gcd_Euclid(b, a % b)
 
+# ---------------- Primes ---------------- #
+
+def sieve_of_eratosthenes(n):
+    """
+    Returns a list of prime numbers up to n
+    """
+    primes = []
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, n + 1):
+        if is_prime[i]:
+            primes.append(i)
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+
+    return primes
+
 # ---------------- Modulo ---------------- #
 # https://sites.math.washington.edu//~greenber/Congruences.pdf
 
+MOD = 998244353
 MOD = 10**9 + 7
 
 
@@ -146,4 +227,4 @@ def nCr_with_mod(n, r):
         numerator = (numerator * (n - i)) % MOD
         denominator = (denominator * (i + 1)) % MOD
 
-    return (numerator * mod_inverse(denominator)) % MOD
+    return (numerator * mod_inverse_prime(denominator)) % MOD
